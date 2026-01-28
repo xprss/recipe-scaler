5dcd63b01bc6b46f697f1dc6c9846e5448a45377
xprss <80033167+xprss@users.noreply.github.com>
2026-01-28 23:12:14 +0100

    added version tag to title

--- frontend/src/app/components/shared/title/title.ts
+++ frontend/src/app/components/shared/title/title.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Button } from 'primeng/button';
+import { environment } from '../../../../environment/environment';
 
 @Component({
   selector: 'app-title',
@@ -8,6 +9,7 @@ import { Button } from 'primeng/button';
   styleUrl: './title.scss',
 })
 export class Title {
+  public version: string = "v" + environment.version;
   public onNavigateToGitHub(): void {
     window.open('https://www.github.com/xprss', '_blank');
   }
