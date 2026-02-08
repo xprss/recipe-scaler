56f6e7b9178aadbc4319dc3d9f73193fef25e076
xprss <80033167+xprss@users.noreply.github.com>
2026-02-08 12:45:15 +0100

    added new langs to array

--- frontend/src/app/lib/langs.ts
+++ frontend/src/app/lib/langs.ts
@@ -10,5 +10,18 @@ export interface Lang {
 
 export const Langs: Lang[] = [
   { langCode: 'it-IT', displayName: '🇮🇹' },
-  { langCode: 'en-US', displayName: '🇬🇧' },
+  { langCode: 'en-UK', displayName: '🇬🇧' },
+  { langCode: 'es-ES', displayName: '🇪🇸' },
+  { langCode: 'fr-FR', displayName: '🇫🇷' },
+  { langCode: 'de-DE', displayName: '🇩🇪' },
+  { langCode: 'ja-JP', displayName: '🇯🇵' },
+  { langCode: 'zh-CH', displayName: '🇨🇳' },
+  { langCode: 'ar-PS', displayName: '🇵🇸' },
+  { langCode: 'da-DK', displayName: '🇩🇰' },
+  { langCode: 'fi-FI', displayName: '🇫🇮' },
+  { langCode: 'hi-IN', displayName: '🇮🇳' },
+  { langCode: 'no-NO', displayName: '🇳🇴' },
+  { langCode: 'pl-PL', displayName: '🇵🇱' },
+  { langCode: 'sv-SE', displayName: '🇸🇪' },
+  { langCode: 'en-US', displayName: '🇺🇸' },
 ];
